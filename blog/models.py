1bbcfd22cdffef575e48fd4d18696d187b1d9901
christine <gacuirichristine@gmail.com>
2020-04-30 12:52:03 +0300

    create custom managers

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,21 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class PublishedManager(models.Manager):
+
+    def get_queryset(self):
+        return super(PublishedManager,self).get_queryset().filter(status='published')
+    
+class TitleStartsWithSManager(models.Manager):
+
+    def get_queryset(self):
+        return super(TitleStartsWithSManager,self).get_queryset().filter(title__startswith='s')
+
+
+
+        
+
 class Post(models.Model):
     STATUS_CHOICES=(
         ("draft","Draft"),
@@ -14,9 +29,12 @@ class Post(models.Model):
     author=models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts')
     body=models.TextField()
     publish=models.DateTimeField(default=timezone.now)
-    created=models.DateTimeField(auto_now_add=True)
+    created=models.DateTimeField(auto_now_add=True) 
     updated=models.DateTimeField(auto_now=True)
     status=models.CharField(max_length=10,choices=STATUS_CHOICES,default="draft")
+    objects=models.Manager()
+    published=PublishedManager()
+    title_startswith=TitleStartsWithSManager()
 
 
     class Meta:
