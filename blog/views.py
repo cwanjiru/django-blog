7ac3f1a251a0389bd062bd31a3e21593f4ade461
christine <gacuirichristine@gmail.com>
2020-05-11 16:19:54 +0300

    Removed class based view.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
-from django.views.generic import ListView
+
 # Create your views here.
 def post_list(request):
     object_list=Post.published.all()
@@ -20,11 +20,7 @@ def post_list(request):
 
 
 
-class PostItemsView(ListView):
-    queryset=Post.published.all()
-    context_object_name='all_posts'
-    paginate_by=3
-    template_name='blog/post/list.html'
+
 
 def post_detail(request,year,month,day,slug):
     post=get_object_or_404(Post,slug=slug,status='published',publish__year=year,publish__month=month,publish__day=day)
