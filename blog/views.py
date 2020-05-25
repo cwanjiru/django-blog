00c17e48df5e45cd6be2bf7e8e6636d0b705ecce
christine <gacuirichristine@gmail.com>
2020-05-25 10:08:12 +0300

    Add similar posts that displays the most similar posts.

--- blog/views.py
+++ blog/views.py
@@ -5,19 +5,19 @@ from .forms import EmailPostForm,CommentForm
 from django.core.mail import send_mail
 from django.conf import settings
 from taggit.models import Tag
+from django.db.models import Count
 
 # Create your views here.
 def post_list(request,tag_slug=None):
     object_list=Post.published.all()
+    if tag_slug:
+        tag=get_object_or_404(Tag,slug=tag_slug)
+        object_list=object_list.filter(tags__in=[tag])
+        
     paginator=Paginator(object_list,3)
     page=request.GET.get('page')
     tag=None
 
-    if tag_slug:
-        tag=get_object_or_404(Tag,slug=tag_slug)
-        object_list=object_list.filter(tags__in=[tag])
-
-     
 
     try:
         posts=paginator.page(page)
@@ -52,9 +52,14 @@ def post_detail(request,year,month,day,slug):
 
     else:
         comment_form=CommentForm()
+    
+
+    post_tags_ids=post.tags.values_list('id',flat=True)
+    similar_posts=Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts=similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
             
 
-    return render(request,'blog/post/detail.html',{'post':post,'comments':comments,'new_comment':new_comment,'comment_form':comment_form})
+    return render(request,'blog/post/detail.html',{'post':post,'comments':comments,'new_comment':new_comment,'comment_form':comment_form,'similar_posts':similar_posts})
     
 
 def post_share(request,post_id):
